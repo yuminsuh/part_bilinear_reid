97a25255a2fb302eaa522945b0e3c6ecf79dc1ae
yumin <n12345@snu.ac.kr>
2018-08-09 19:50:58 +0900

    set path util

--- reid/utils/osutils.py
+++ reid/utils/osutils.py
@@ -9,3 +9,15 @@ def mkdir_if_missing(dir_path):
     except OSError as e:
         if e.errno != errno.EEXIST:
             raise
+
+def set_paths(paths_file=None, verbose=True):
+    if not paths_file:
+        raise ValueError('Set paths file')
+    print('Set os.envirionments from {}...'.format(paths_file))
+    if verbose: print('----------------------------------')
+    paths_dict = {v.split('=')[0]:v.split('=')[1] for v in  open(paths_file,'r').read().split()}
+    for path, v in paths_dict.items():
+        os.environ[path] = v
+        if verbose: print(path, v)
+    if verbose: print('----------------------------------')
+    print('done!')
