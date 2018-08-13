3ab3a68f19c9d55654ef797a8aa7e2f6129ea06a
yumin <n12345@snu.ac.kr>
2018-08-13 11:45:11 +0900

    evaluation script

---
+++ eval_market1501.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Download pretrained network weights
+if ! [ -d "logs/market1501/d2_b250" ]; then
+    mkdir -p "logs/market1501/d2_b250"
+fi
+if ! [ -f "logs/market1501/d2_b250/args.json" ]; then
+    wget "https://www.dropbox.com/s/e0cy15io2pwuzth/args.json?dl=0" -O "logs/market1501/d2_b250/args.json"
+fi
+if ! [ -f "logs/market1501/d2_b250/epoch_750.pth.tar" ]; then
+    wget "https://www.dropbox.com/s/dugjdeav6iapjvt/epoch_750.pth.tar?dl=0" -O "logs/market1501/d2_b250/epoch_750.pth.tar"
+    
+fi
+
+python test.py
